4407a3cede38c23bde28ba1280c44f0dcac4e59e
gat6ns <163240470+gat6ns@users.noreply.github.com>
2024-05-16 00:03:46 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,217 +1,164 @@
 <!DOCTYPE html>
 <html lang="en">
+<head>
+<title>PORTFOLIO</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" type='text/css' href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css"/>
+<link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
+<style>
+body {font-family: "Lato", sans-serif}
 
-  <head>
+.mySlides {display: none}
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Resume</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom fonts for this template -->
-    <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:100,200,300,400,500,600,700,800,900" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
-    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-    <link href="vendor/devicons/css/devicons.min.css" rel="stylesheet">
-    <link href="vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet">
-    <link rel="stylesheet" type='text/css' href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css" />
-
-    <!-- Custom styles for this template -->
-    <link href="css/resume.min.css" rel="stylesheet">
-
-  </head>
-
-  <body id="page-top">
-
-  <style>
-    /*!
- * Start Bootstrap - Resume v4.0.0-beta.2 (https://startbootstrap.com/template-overviews/resume)
- * Copyright 2013-2017 Start Bootstrap
- * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-resume/blob/master/LICENSE)
- */
-body {
-  font-family: 'Open Sans', serif;
-  padding-top: 54px;
-  color: #868e96; }
-
-@media (min-width: 992px) {
-  body {
-    padding-top: 0;
-    padding-left: 17rem; } }
-
-h1,
-h2,
-h3,
-h4,
-h5,
-h6 {
+h2{
   font-family: 'Saira Extra Condensed', serif;
   font-weight: 700;
   text-transform: uppercase;
-  color: #343a40; }
+  color: #343a40;
+  }
 
-h1 {
-  font-size: 6rem;
-  line-height: 5.5rem; }
-
-h2 {
-  font-size: 3.5rem; }
-
-.subheading {
+  .w3-top{
+  font-family: "Lato", sans-serif;
+  font-weight: 700;
   text-transform: uppercase;
-  font-weight: 500;
+  color: #ffffff;
+  }
+
+  p{
+  font-family: "Lato", sans-serif;
+  }
+  
+  .w3-bar{
+  background-color: #000000;
+  }
+
+  .w3-third{
   font-family: 'Saira Extra Condensed', serif;
-  font-size: 1.35rem; }
+  font-weight: 700;
+  }
+  
+  img {
+    border-radius: 50%;
+  }
 
-.list-social-icons a {
-  color: #495057; }
-  .list-social-icons a:hover {
-    color: #BD5D38; }
-  .list-social-icons a .fa-lg {
-    font-size: 1.75rem; }
+</style>
+</head>
+<body>
 
-.list-icons {
-  font-size: 3rem; }
-  .list-icons .list-inline-item i:hover {
-    color: #BD5D38; }
-
-#sideNav .navbar-nav .nav-item .nav-link {
-  font-weight: 600;
-  text-transform: uppercase; }
-
-@media (min-width: 992px) {
-  #sideNav {
-    text-align: center;
-    position: fixed;
-    top: 0;
-    left: 0;
-    display: flex;
-    flex-direction: column;
-    width: 17rem;
-    height: 100vh; }
-    #sideNav .navbar-brand {
-      display: flex;
-      margin: auto auto 0;
-      padding: 0.5rem; }
-      #sideNav .navbar-brand .img-profile {
-        max-width: 10rem;
-        max-height: 10rem;
-        border: 0.5rem solid rgba(255, 255, 255, 0.2); }
-    #sideNav .navbar-collapse {
-      display: flex;
-      align-items: flex-start;
-      flex-grow: 0;
-      width: 100%;
-      margin-bottom: auto; }
-      #sideNav .navbar-collapse .navbar-nav {
-        flex-direction: column;
-        width: 100%; }
-        #sideNav .navbar-collapse .navbar-nav .nav-item {
-          display: block; }
-          #sideNav .navbar-collapse .navbar-nav .nav-item .nav-link {
-            display: block; } }
-
-section.resume-section {
-  border-bottom: 1px solid #dee2e6;
-  padding-top: 5rem !important;
-  padding-bottom: 5rem !important; }
-  section.resume-section .resume-item .resume-date {
-    min-width: none; }
-
-@media (min-width: 768px) {
-  section.resume-section {
-    min-height: 100vh; }
-    section.resume-section .resume-item .resume-date {
-      min-width: 18rem; } }
-
-@media (min-width: 992px) {
-  section.resume-section {
-    padding-top: 3rem !important;
-    padding-bottom: 3rem !important; } }
-
-.bg-primary {
-  background-color: #BD5D38 !important; }
-
-.text-primary {
-  color: #BD5D38 !important; }
-
-a {
-  color: #BD5D38; }
-  a:hover, a:focus, a:active {
-    color: #824027; }
-
-  </style>
-
-    <div class="container-fluid p-0">
-
-      <section class="resume-section p-3 p-lg-5 d-flex d-column" id="about">
-        <div class="my-auto">
-          <h1 class="mb-0">Jewell
-            <span class="text-primary">Estillore</span>
-          </h1>
-          <div class="subheading mb-5">La Union, Philippines
-          </div>
-          <p class="mb-5">Hello! I’m Jewell, a passionate graphic designer and video editor. With a keen eye for detail and a love for visual storytelling, I specialize in creating compelling visuals that captivate and engage audiences. Proficient in Adobe After Effects and Adobe Photoshop, I bring creative ideas to life, whether it’s through stunning graphics or dynamic video content.</p>
-        </div>
-      </section>
-
-      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="skills">
-        <div class="my-auto">
-          <h2 class="mb-5">Skills</h2>
-
-          <div class="subheading mb-3">Softwares i use</div>
-          <ul class="list-inline list-icons">
-            <li class="list-inline-item">
-              <i class="devicon-aftereffects-plain colored"></i>
-            </li>
-            <li class="list-inline-item">
-              <i class="devicon-photoshop-plain colored"></i>
-          </ul>
-
-          <div class="subheading mb-3">Workflow</div>
-          <ul class="fa-ul mb-0">
-            <li>
-              <i class="fa-li fa fa-check"></i>
-              Graphic Design: Expert in Adobe Photoshop, skilled in creating logos, posters, social media graphics, and more.</li>
-            <li>
-              <i class="fa-li fa fa-check"></i>
-              Video Editing: Proficient in Adobe After Effects, specializing in motion graphics, visual effects, and video post-production.</li>
-          </ul>
-        </div>
-      </section>
-
-      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="interests">
-        <div class="my-auto">
-          <h2 class="mb-5">Interests</h2>
-          <p>Graphic Design
-            Graphic design is at the heart of what I do. I love experimenting with different styles, color palettes, and typography to create visually appealing and impactful designs. Whether it's creating logos, posters, or social media graphics, I find joy in every aspect of the design process. <br><br>
-            
-            Video Editing
-            Video editing allows me to combine my passion for storytelling with my technical skills. I enjoy crafting engaging narratives through seamless transitions, dynamic effects, and creative animations. Working with Adobe After Effects and Premiere Pro, I strive to produce videos that not only look great but also convey a compelling story. <br><br>
-            
-            Motion Graphics
-            I have a keen interest in motion graphics, where I can blend my graphic design skills with animation. Creating animated explainer videos, promotional content, and visually captivating intros gives me an opportunity to push the boundaries of creativity.</p>
-          <p class="mb-0">Thank you for visiting my portfolio! I look forward to the opportunity to collaborate and create something amazing together.</p>
-        </div>
-      </section>
-
+<!-- Navbar -->
+<div class="w3-top">
+  <div class="w3-bar w3-card">
+    <a class="w3-bar-item w3-button w3-padding-large w3-hide-medium w3-hide-large w3-right" href="javascript:void(0)" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
+    <a href="#" class="w3-bar-item w3-button w3-padding-large">HOME</a>
+    <a href="#band" class="w3-bar-item w3-button w3-padding-large w3-hide-small">ABOUT ME</a>
+    <a href="#tour" class="w3-bar-item w3-button w3-padding-large w3-hide-small">SKILLS</a>
+    <a href="#contact" class="w3-bar-item w3-button w3-padding-large w3-hide-small">INTEREST</a>
+    <div class="w3-dropdown-hover w3-hide-small">
     </div>
+  </div>
+</div>
 
-    <!-- Bootstrap core JavaScript -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-    <!-- Plugin JavaScript -->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+<!-- Page content -->
+<div class="w3-content" style="max-width:2000px;margin-top:46px">
 
-    <!-- Custom scripts for this template -->
-    <script src="js/resume.min.js"></script>
+  <!-- The Band Section -->
+  <div class="w3-container w3-content w3-center w3-padding-64" style="max-width:800px" id="band">
+    <img src="profile.jpg" alt="profile" style="width:10%" class="w3-hover-opacity">
+    <h2 class="w3-wide w3-center"  style="font-size:70px;">JEWELL ESTILLORE</h2>
+    <p class="w3-opacity"><i>LA UNION, PHILIPPINES</i></p>
+    <p class="w3-opacity" >Hello! I’m Jewell, a passionate graphic designer and video editor. With a keen eye for detail and a love for visual storytelling, I specialize in creating compelling visuals that captivate and engage audiences. Proficient in Adobe After Effects and Adobe Photoshop, I bring creative ideas to life, whether it’s through stunning graphics or dynamic video content.</p>
+    <div class="w3-row w3-center w3-padding-32">
+      <div class="w3-third">
+        <p>After Effects</p>
+        <i class="devicon-aftereffects-plain colored"></i>
+      </div>
+      <div class="w3-third">
+        <p>Photoshop</p>
+        <i class="devicon-photoshop-plain colored"></i>
+      </div>
+      <div class="w3-third">
+        <p>Premiere Pro</p>
+        <i class="devicon-premierepro-plain colored"></i>
+      </div>
+    </div>
+  </div>
 
-  </body>
+  <!-- The Tour Section -->
+  <div class="w3-black" id="tour">
+    <div class="w3-container w3-content w3-padding-64" style="max-width:800px">
+      <h2 class="w3-wide w3-center">SKILLS</h2>
+      <p class="w3-opacity w3-center"><i>WORKFLOW
+        Graphic Design: Expert in Adobe Photoshop, skilled in creating logos, posters, social media graphics, and more.
+        Video Editing: Proficient in Adobe After Effects, specializing in motion graphics, visual effects, and video post-production.</i></p><br>
+    </div>
+  </div>
 
+  <!-- The Contact Section -->
+  <div class="w3-container w3-content w3-padding-64" style="max-width:800px" id="contact">
+    <h2 class="w3-wide w3-center">INTEREST</h2>
+    <p class="w3-opacity w3-center"><i>Graphic Design Graphic design is at the heart of what I do. I love experimenting with different styles, color palettes, and typography to create visually appealing and impactful designs. Whether it's creating logos, posters, or social media graphics, I find joy in every aspect of the design process. <br><br>
+
+        Video Editing Video editing allows me to combine my passion for storytelling with my technical skills. I enjoy crafting engaging narratives through seamless transitions, dynamic effects, and creative animations. Working with Adobe After Effects and Premiere Pro, I strive to produce videos that not only look great but also convey a compelling story. <br><br>
+        
+        Motion Graphics I have a keen interest in motion graphics, where I can blend my graphic design skills with animation. Creating animated explainer videos, promotional content, and visually captivating intros gives me an opportunity to push the boundaries of creativity. <br><br>
+        
+        Thank you for visiting my portfolio! I look forward to the opportunity to collaborate and create something amazing together.</i></p>
+  </div>
+  
+<!-- End Page Content -->
+</div>
+
+<!-- Image of location/map -->
+<img src="/w3images/map.jpg" class="w3-image w3-greyscale-min" style="width:100%">
+
+<!-- Footer -->
+<footer class="w3-container w3-padding-64 w3-center w3-opacity w3-light-grey w3-xlarge">
+  <i class="fa fa-instagram w3-hover-opacity"></i>
+  <i class="fa fa-pinterest w3-hover-opacity"></i>
+  <p class="w3-medium">Jewell Estillore</a></p>
+</footer>
+
+<script>
+// Automatic Slideshow - change image every 4 seconds
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 4000);    
+}
+
+// Used to toggle the menu on small screens when clicking on the menu button
+function myFunction() {
+  var x = document.getElementById("navDemo");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+// When the user clicks anywhere outside of the modal, close it
+var modal = document.getElementById('ticketModal');
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+</script>
+
+</body>
 </html>
